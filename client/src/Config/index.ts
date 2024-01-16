230895acdb416e0133d5aef9646c2d35a82da39f
anandvikasog <vikas.anand@ongraph.com>
2024-01-16 12:35:05 +0530

    deployed api

--- client/src/Config/index.ts
+++ client/src/Config/index.ts
@@ -1,1 +1,5 @@
-export const API_URI = "http://localhost:8000"
+console.log(process.env.NODE_ENV);
+export const API_URI =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "https://react-advanced-backend.onrender.com";
--- client/src/utils/sections.ts
+++ client/src/utils/sections.ts
@@ -1,7 +1,7 @@
-// import Section2 from "../Components/Section2";
-// import Section3 from "../Components/Section3";
-// import Section4 from "../Components/Section4";
-// import Section5 from "../Components/Section5";
+import Section2 from "../Components/Section2";
+import Section3 from "../Components/Section3";
+import Section4 from "../Components/Section4";
+import Section5 from "../Components/Section5";
 import Section6 from "../Components/Section6";
 
 import { SectionList } from "../Types";
@@ -12,19 +12,19 @@ export const sections: SectionList = [
   },
   {
     title: "Section 2 (Design patterns: Layout components)",
-    // component: Section2,
+    component: Section2,
   },
   {
     title: "Section 3 (Design patterns: Container components)",
-    // component: Section3,
+    component: Section3,
   },
   {
     title: "Section 4 (Controlled and Uncontrolled Components)",
-    // component: Section4,
+    component: Section4,
   },
   {
     title: "Section 5 (Design patterns: HOCs)",
-    // component: Section5,
+    component: Section5,
   },
   {
     title: "Section 6 (Design patterns: Custom hooks)",
